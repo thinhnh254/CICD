e30423ec75cfdb7f648d07d4e1cf7a7e95a22cdd
thinhnh254 <nguyenhungthinh2504@gmail.com>
2024-06-20 16:45:15 +0700

    update test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,6 +10,6 @@ export async function generateMetadata({ params }: Params) {
 
 export default function Page({ params }: Params) {
   return <h1>Slug: {params.slug} 
-  <p>TomNguyen</p> 
+  <p>Tom Nguyen</p> 
   <p>Test</p></h1>;
 }
